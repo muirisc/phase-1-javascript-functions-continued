0c14e80511cabb95b7ba03b13872cc8d7fad8a87
muirisc <91299876+muirisc@users.noreply.github.com>
2021-11-02 00:12:04 -0400

    commit tree e4f2be81be855738d8f1cd6c07e3e018aa35d209

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Your code here
+function saturdayFun(activity = "roller-skate"){
+return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(activity = "go to the office"){
+
+    return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(flair= "*"){
+
+    return function inner(test = "special"){
+       return `You are ${flair}${test}${flair}!`
+   }
+
+} wrapAdjective("||")("a dedicated programmer")
